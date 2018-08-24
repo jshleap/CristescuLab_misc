32acd8064a3004678561180b4ee7c3b3eced0545
jshleap <jshleap@squalus.org>
2018-08-24 10:01:17 -0400

    Including workaround on species parsing

--- useful_scripts/blast_processing.py
+++ useful_scripts/blast_processing.py
@@ -39,7 +39,19 @@ from subprocess import Popen, PIPE
 SIX = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
 
 
-def parse_blast(fn, filters={}, top_n_hits=None, output_filtered=False):
+def get_sps(line, coi=False):
+    if coi:
+        return line.split('|')[1]
+    elif '|' in line:
+        return ' '.join(line.split('|')[2].split()[:2])
+    elif 'Fish' in line:
+       return line.split()[0]
+    else:
+        return ' '.join(line.split()[1:3])
+
+
+def parse_blast(fn, filters={}, top_n_hits=None, output_filtered=False,
+                coi=False):
     """
     Parse a blast file, and filter it if required
 
@@ -58,12 +70,13 @@ def parse_blast(fn, filters={}, top_n_hits=None, output_filtered=False):
         names = 'qseqid sseqid pident evalue qcovs stitle'.split()
         by = 'evalue pident qcovs'.split()
         asc = [True, False, False]
-    df.rename(dict(zip(range(len(names)), names)), inplace=True)
+    df.rename(columns=dict(zip(range(len(names)), names)), inplace=True)
     if filters:
         query = ' & '.join(
             ['(%s > %d)' % (k, v) if k != 'evalue' else '(%s < %e)' % (k, v)
              for k, v in filters.items()])
         df = df.query(query)
+    print(df.head())
     if top_n_hits is not None:
         args = dict(by=by, ascending=asc)
         df = df.sort_values(**args).groupby('qseqid').head(top_n_hits)
@@ -77,8 +90,8 @@ def parse_blast(fn, filters={}, top_n_hits=None, output_filtered=False):
         df = pd.concat([df, ndf], axis=1)
     else:
         # Assume that species is in the first two fields of stitle
-        def get_sp(x): return ' '.join(x.strip().split()[:2])
-        df['species'] = df.stitle.apply(get_sp)
+        #def get_sps(x): return ' '.join(x.strip().split()[:2])
+        df['species'] = df.stitle.apply(get_sps, coi=coi)
     if output_filtered:
         outfn = fn[:fn.rfind('.')]
         df.to_csv(outfn, sep='\t', index=False, header=False)
@@ -102,7 +115,7 @@ def get_reads_per_group(df, taxlevel='species', min_reads=10):
     re = df.groupby('qseqid')[taxlevel].nunique()
     re.to_csv('Numer_of_reads_in_%s.tsv' % taxlevel, sep='\t')
     # List number of unique species above the min_reads
-    sps = cou[cou.qseqid > min_reads].index
+    sps = cou[cou > min_reads].index.unique().to_series()
     sps.to_csv('List_unique_%s' % taxlevel, header=False, index=False)
 
 
@@ -119,16 +132,16 @@ def plot_tax(df, n, taxlevel='species', tax_for_pattern=None, pattern=None,
     """
     prefix = 'top%dhits_%s' % (n, taxlevel)
     if pattern is not None:
-        final = df[df.loc[:, tax_for_pattern].str.contains(pattern)]
+        df = df[df.loc[:, tax_for_pattern].str.contains(pattern)]
         prefix += '_%s' % pattern
     if suffix is not None:
         prefix += '_%s' % suffix
-    cols = ['Pond', taxlevel, 'qaccver']
-    toplot = final.reindex(columns=cols)
+    cols = [taxlevel, 'qseqid']
+    toplot = df.reindex(columns=cols)
     ntaxa = toplot.loc[:, taxlevel].nunique()
     color = matplotlib.cm.inferno_r(np.linspace(.0, 1., ntaxa))
     toplot = toplot.groupby([taxlevel]).count()
-    toplot = toplot[toplot.qaccver > min_reads].qaccver.unstack()
+    toplot = toplot[toplot.qseqid > min_reads].unstack()
     fig, ax = plt.subplots()
     toplot.plot(kind='bar', stacked=False, color=color, ax=ax)
     ax.set_xlabel("Sites")
@@ -141,8 +154,9 @@ def plot_tax(df, n, taxlevel='species', tax_for_pattern=None, pattern=None,
 def main(blast_file, pident=None, eval=None, qcov=None, qlen=None, length=None,
          output_filtered=False, taxlevel='species', min_reads=0, plot=False,
          tax_for_pattern=None, pattern=None, suffix_for_plot=None, ntop=None):
-    filters = dict(zip(['pident', 'eval', 'qcov', 'qlen', 'length'],
+    filters = dict(zip('pident evalue qcovs qlen length'.split(),
                        [pident,   eval,   qcov,   qlen,   length]))
+    filters = {k: v for k, v in filters.items() if v is not None}
     kwargs = dict(filters=filters, output_filtered=output_filtered,
                   top_n_hits=ntop)
     df = parse_blast(blast_file, **kwargs)
@@ -154,8 +168,6 @@ def main(blast_file, pident=None, eval=None, qcov=None, qlen=None, length=None,
 
 if __name__ == '__main__':
     opts = optparse.OptionParser(usage='%prog [options] blast_file')
-    opts.add_option('--fasta_fn', '-f', action='store',
-                    help='Fasta file to process')
     opts.add_option('--output_filtered', '-o', action='store_true',
                     default=False, help=('Output a TSV with the filtered table'
                                          ' [default: %default]'))
@@ -169,7 +181,7 @@ if __name__ == '__main__':
                     help='Minimum query length [default: %default]')
     opts.add_option('--length', '-l', action='store', type=int, default=None,
                     help='Minimum alignment length [default: %default]')
-    opts.add_option('--taxlevel', '-t', action='store',default='species',
+    opts.add_option('--taxlevel', '-t', action='store', default='species',
                     help='Taxonomic level to display [default: %default]')
     opts.add_option('--min_reads', '-r', action='store', type=int, default=0,
                     help=('Minimum number of reads to retain group '
@@ -183,11 +195,15 @@ if __name__ == '__main__':
                     help=('Pattern to subset the tax_for_pattern with '
                           '[default: %default]'))
     opts.add_option('--suffix_for_plot', '-s', default=None,
-                    help='Suffix for plot (before extension) [default: %default]')
+                    help=('Suffix for plot (before extension) '
+                          '[default: %default]'))
     opts.add_option('--ntop', '-n', action='store', type=int, default=None,
                     help='Number of hits per query [default: %default]')
+    opts.add_option('--use_coi', '-c', action='store_true', default=False,
+                    help=('If no special formating in teh database and using'
+                          ' COI [default: %default]'))
 
     opt, arg = opts.parse_args()
-    main(arg[1], opt.pident, opt.eval, opt.qcov, opt.qlen, opt.length,
+    main(arg[0], opt.pident, opt.eval, opt.qcov, opt.qlen, opt.length,
          opt.output_filtered, opt.taxlevel, opt.min_reads, opt.plot,
          opt.tax_for_pattern, opt.pattern, opt.suffix_for_plot, opt.ntop)
