91c96d1bf7850b9ead7b7e5bb513ac8996f0e14c
jshleap <jshleap@squalus.org>
2018-09-07 18:09:15 -0400

    Fixng the missing file

--- useful_scripts/get_stats_per_stage.py
+++ useful_scripts/get_stats_per_stage.py
@@ -7,7 +7,7 @@ from joblib import Parallel, delayed
 from tqdm import tqdm
 import sys
 
-
+COLS = 'prefix type num_seqs min_len avg_len max_len'.split()
 
 def get_prefix(lis):
     first_el = lis.pop(0)
@@ -24,16 +24,16 @@ def loop(d):
     #o, e = seqkit.communicate()
     files = glob('%s/*.fast?' % d)
     o = check_output(['seqkit', 'stats'] + files)
-    df = pd.read_table(StringIO(o.decode()), delim_whitespace=True)
+    df = pd.read_table(StringIO(o.decode()), delim_whitespace=True, thousands=',')
     df['prefix'] = get_prefix(df.file)
     df['type'] = df.file.apply(lambda x: '.'.join(x.split('.')[-2:]))
-    df = df.reindex(columns = 'prefix type num_seqs min_len avg_len max_len'.split())
-    df = pd.pivot_table(df, values='num_seqs  min_len  avg_len  max_len'.split(),
-                        index='prefix', columns=['type'])
+    df = df.reindex(columns=COLS)
+    df = pd.pivot_table(df, values=COLS[2:], index=['prefix'], columns=['type'])
     return df
 
+
 def writedf(df, col):
-    df.to_csv('%s_%s.tsv' % (sys.argv[1], col), sep='\t')
+    df.loc[:,col].to_csv('%s_%s.tsv' % (sys.argv[1], col), sep='\t')
 
 
 dirs = glob('%s*/' % sys.argv[1])
