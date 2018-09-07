2e8d6749b20c53534daeb1b273807506d49c7a76
jshleap <jshleap@squalus.org>
2018-09-07 15:18:10 -0400

    include loop over folders

--- useful_scripts/get_stats_per_stage.py
+++ useful_scripts/get_stats_per_stage.py
@@ -2,6 +2,8 @@ import pandas as pd
 from io import StringIO
 from subprocess import Popen, PIPE
 from difflib import SequenceMatcher
+from glob import iglob
+import sys
 
 def get_prefix(lis):
     first_el = lis.pop(0)
@@ -11,17 +13,20 @@ def get_prefix(lis):
         first_el = first_el[match.a: match.a + match.size]
     return first_el
 
+dirs = iglob('%s*/' % sys.argv[1])
 
-line = 'seqkit stats *.fast*'
-seqkit = Popen(line, shell=True, stdout=PIPE, stderr=PIPE)
-o, e = seqkit.communicate()
-df = pd.read_table(StringIO(o.decode()), delim_whitespace=True)
-df['prefix'] = get_prefix(df.file)
-df['type'] = df.file.apply(lambda x: '.'.join(x.split('.')[-2:]))
-df = df.reindex(columns = 'prefix type num_seqs min_len avg_len max_len'.split())
-df = pd.pivot_table(df, values='num_seqs  min_len  avg_len  max_len'.split(),
-                    index='prefix', columns=['type'])
+alldfs = []
+for d in dirs:
+    line = 'seqkit stats %s*.fast*' % d
+    seqkit = Popen(line, shell=True, stdout=PIPE, stderr=PIPE)
+    o, e = seqkit.communicate()
+    df = pd.read_table(StringIO(o.decode()), delim_whitespace=True)
+    df['prefix'] = get_prefix(df.file)
+    df['type'] = df.file.apply(lambda x: '.'.join(x.split('.')[-2:]))
+    df = df.reindex(columns = 'prefix type num_seqs min_len avg_len max_len'.split())
+    df = pd.pivot_table(df, values='num_seqs  min_len  avg_len  max_len'.split(),
+                        index='prefix', columns=['type'])
+    alldfs.append(df)
+df = pd.concat(df)
 for i in 'min_len avg_len max_len'.split():
-    sub = df.loc[:, i]
-    pref = sub.index.tolist()[0]
-    sub.to_csv('%s_%s.tsv' % (pref, i), sep='\t')
+    df.to_csv('%s_%s.tsv' % (sys.argv[1], i), sep='\t')
